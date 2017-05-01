6e11db5b90526babb2beadf5ff3df2a9b7f84cb5
BrianSPerry <brianperry916@hotmail.co.uk>
2017-04-30 20:05:49 -0400

    Implemeting Test for the Video cover

---
+++ script/videoscript.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+	var vid = $('video');
+	var vid_w_orig = 1280;
+	var vid_h_orig = 720;
+	
+	$(window).resize(function(){
+	
+	var container_w = vid.parent().width();
+	var container_h = vid.parent().height();
+	
+	var scale_w = container_w / vid_w_orig;
+	var scale_h = container_h / vid_h_orig;
+	var scale = scale_w > scale_h ? scale_w : scale_h;
+		
+	vid.width(scale * vid_w_orig);
+	vid.height(scale * vid_h_orig);
+			
+	});
+	
+	$(window).trigger('resize');
+});
